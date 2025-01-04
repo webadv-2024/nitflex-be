a725c3dc7c34f912a65efc3943bf7265de91c68c
hkhangus <huukhangtc@gmail.com>
2025-01-04 14:49:03 +0700

    feat: auth middleware

---
+++ internal/middleware/auth.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"net/http"
+	"nitflex/util"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, util.FailResponse("authorization header is required"))
+			c.Abort()
+			return
+		}
+
+		// Check if the header starts with "Bearer "
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, util.FailResponse("invalid authorization header format"))
+			c.Abort()
+			return
+		}
+
+		// Validate the token
+		claims, err := util.Verify(c, parts[1])
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, util.FailResponse("invalid token"))
+			c.Abort()
+			return
+		}
+
+		// Store user information in the context
+		c.Set("user_id", claims)
+
+		c.Next()
+	}
+} 
