0eb57deed961b00247dfa9d96416854c5c5a40b4
hkhangus <huukhangtc@gmail.com>
2025-01-13 22:30:15 +0700

    chore: api get rating add movie info

--- internal/handler/business/ratings.go
+++ internal/handler/business/ratings.go
@@ -17,11 +17,31 @@ func (b *business) UpdateRating(ctx context.Context, userID string, movieID stri
 }
 
 func (b *business) GetRatingUser(ctx context.Context, userID string) (*models.RatingsResponse, error) {
-	rating, err := b.repo.GetRatingUser(ctx, userID)
+	ratings, err := b.repo.GetRatingUser(ctx, userID)
 	if err != nil {
 		return nil, err
 	}
+
+	results := make([]*models.RatingWithMovie, 0, len(ratings))
+	for _, r := range ratings {
+		movie, err := b.repo.GetMovieByID(ctx, r.MovieId)
+		if err != nil {
+			return nil, err
+		}
+	
+		ratingWithMovie := &models.RatingWithMovie{
+			Rating: r,
+			Movie: &models.MovieRating{
+				ID:          movie.TmdbId,
+				Title:       movie.Title,
+				Overview:    movie.Overview,
+				PosterPath:  movie.PosterPath,
+			},
+		}
+		results = append(results, ratingWithMovie)
+	}
+
 	return &models.RatingsResponse{
-		Results: rating,
+		Results: results,
 	}, nil
 }
--- internal/models/rating.go
+++ internal/models/rating.go
@@ -2,6 +2,18 @@ package models
 
 import "nitflex/internal/repository"
 
+type MovieRating struct {
+	ID          int    `bson:"id"`
+	Title       string `bson:"title"`
+	Overview    string `bson:"overview"`
+	PosterPath  string `bson:"poster_path"`
+}
+
+type RatingWithMovie struct {
+	*repository.Rating
+	Movie *MovieRating `bson:"movie"`
+}
+
 type RatingsResponse struct {
-	Results []*repository.Rating `bson:"results"`
+	Results []*RatingWithMovie `bson:"results"`
 }
